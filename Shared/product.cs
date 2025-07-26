b018a4cc39f211d7742e2a93ddd1551937a09686
niloufar <niloufar.ebrahimizade@gmail.com>
2025-07-26 06:50:15 +0330

    Add project files.

---
+++ Shared/product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorApp2.Shared
+{
+    public class Product
+    {
+        public int  Id { get; set; }
+        public string Titel { get; set; }=string.Empty; 
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; }
+        public  decimal Price{ get; set; }
+
+    }
+}
